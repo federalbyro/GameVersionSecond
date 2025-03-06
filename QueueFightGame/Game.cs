7c53cc8e63cead750eaf01cf521b511de6f96beb
Gorney-Alex <gorneyalexandr@gmail.com>
2025-03-06 09:21:03 +0300

    Start class Game

---
+++ QueueFightGame/Game.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueFightGame
+{
+    internal class Game
+    {
+        private GameManager Manager;
+
+        public Game()
+        {
+            Manager = new GameManager();
+        }
+
+        public void Initialize()
+        {
+            throw new NotImplementedException("Метод Initialize() не реализован");
+        }
+
+        public void Play()
+        {
+            throw new NotImplementedException("Метод Play() не реализован");
+        }
+
+
+    }
+}
